b29130f5aee63356530ba27f300f91e1cecd1789
cristobal <chermidap@gmail.com>
2016-02-03 19:48:31 +0100

    Define view elements

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/MainActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/MainActivity.java
@@ -3,17 +3,29 @@ package es.craftsmanship.toledo.katangapp.activities;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * @author Cristóbal Hermida
  */
 public class MainActivity extends Activity {
 
+    private SeekBar seekBar;
+    private TextView txtKatangaLabel;
+    private ImageView button;
+    private TextView txtradiolabel;
+    private ProgressBar progressBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
     }
 
 }
