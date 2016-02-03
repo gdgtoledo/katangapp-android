3351da194e68efb663008664f5687078d9299984
cristobal <chermidap@gmail.com>
2016-02-03 20:22:15 +0100

    Add methods extended from android.app.Service and LocationListener

--- app/src/main/java/es/craftmanship/toledo/katangapp/utils/GPSTracker.java
+++ app/src/main/java/es/craftmanship/toledo/katangapp/utils/GPSTracker.java
@@ -1,8 +1,41 @@
 package es.craftmanship.toledo.katangapp.utils;
 
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by cristobal on 3/2/16.
  */
-public class GPSTracker {
+public class GPSTracker extends Service implements LocationListener {
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
