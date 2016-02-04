7ae263206917062a2348f8f806310d1dbce36c06
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-02-04 09:11:14 +0100

    SF: do not add a blank line in empty methods

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,5 +3,8 @@
 ## File endings
 Do not add a blank line after the last line with content.
 
+## Empty methods
+Do not add a blank line in an empty method.
+
 ## Organize imports
 Separate imports with a blank line on those ones differing their second package names.
--- app/src/main/java/es/craftmanship/toledo/katangapp/utils/GPSTracker.java
+++ app/src/main/java/es/craftmanship/toledo/katangapp/utils/GPSTracker.java
@@ -19,22 +19,18 @@ public class GPSTracker extends Service implements LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
-
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-
     }
 
     @Nullable
