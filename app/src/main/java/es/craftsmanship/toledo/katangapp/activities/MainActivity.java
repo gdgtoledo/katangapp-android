bc610ee56ad532275f57a6cf668e671fa62af769
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-02-04 10:23:20 +0100

    SF: Import the class

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/MainActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -45,7 +46,7 @@ public class MainActivity extends Activity {
 
         initializeVariables();
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
             int progress = 0;
 
             @Override
