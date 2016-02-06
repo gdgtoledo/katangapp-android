b6fe8d570e48cb3eeb427916f0fd06d7a63f1fab
Javier Gamarra <nhpatt@gmail.com>
2016-02-06 23:45:00 +0100

    switch with default

--- app/src/main/java/es/craftsmanship/toledo/katangapp/utils/KatangaFont.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/utils/KatangaFont.java
@@ -1,31 +1,32 @@
 package es.craftsmanship.toledo.katangapp.utils;
 
 import android.content.res.AssetManager;
-
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 
 /**
  * @author Manuel de la Peña
  */
 public enum KatangaFont {
 
-   QUICKSAND_BOLD, QUICKSAND_LIGHT, QUICKSAND_REGULAR;
+    QUICKSAND_BOLD, QUICKSAND_LIGHT, QUICKSAND_REGULAR;
 
     public static Typeface getFont(AssetManager assetManager, KatangaFont font) {
-        String fontPath = "fonts/Quicksand-Regular.ttf";
-
+        String fontPath = getFontPath(font);
+        return Typeface.createFromAsset(assetManager, fontPath);
+    }
+    
+    @NonNull
+    private static String getFontPath(KatangaFont font) {
         switch (font) {
             case QUICKSAND_BOLD:
-                fontPath = "fonts/Quicksand-Bold.ttf";
-                break;
+                return "fonts/Quicksand-Bold.ttf";
             case QUICKSAND_LIGHT:
-                fontPath = "fonts/Quicksand-Light.ttf";
-                break;
+                return "fonts/Quicksand-Light.ttf";
             case QUICKSAND_REGULAR:
-                fontPath = "fonts/Quicksand-Regular.ttf";
+            default:
+                return "fonts/Quicksand-Regular.ttf";
         }
-
-        return Typeface.createFromAsset(assetManager, fontPath);
     }
 
 }
