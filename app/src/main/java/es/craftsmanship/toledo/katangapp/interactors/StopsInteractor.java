376e6d8bcfdac2f942cb7863769ac60abefb77d1
Javier Gamarra <nhpatt@gmail.com>
2016-03-21 18:57:30 +0100

    extract business logic to StopsInteractor

---
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/interactors/StopsInteractor.java
@@ -0,0 +1,50 @@
+package es.craftsmanship.toledo.katangapp.interactors;
+
+import java.io.IOException;
+
+import es.craftsmanship.toledo.katangapp.models.QueryResult;
+import es.craftsmanship.toledo.katangapp.services.StopsService;
+import es.craftsmanship.toledo.katangapp.utils.AndroidBus;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+/**
+ * @author Javier Gamarra
+ */
+public class StopsInteractor implements Runnable {
+
+    private static final String BACKEND_ENDPOINT = "https://secret-depths-4660.herokuapp.com";
+    private final String radio;
+    private final Double latitude;
+    private final Double longitude;
+
+    public StopsInteractor(String radio, Double latitude, Double longitude) {
+        this.radio = radio;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BACKEND_ENDPOINT)
+                    .addConverterFactory(JacksonConverterFactory.create())
+                    .build();
+
+            StopsService service = retrofit.create(StopsService.class);
+
+            Response<QueryResult> response = service.listStops(latitude, longitude, radio).execute();
+
+            if (response.isSuccess()) {
+                AndroidBus.getInstance().post(response.body());
+            } else {
+                AndroidBus.getInstance().post(new Error(response.message()));
+            }
+
+        } catch (IOException e) {
+            AndroidBus.getInstance().post(e);
+        }
+    }
+}
