fb4751d83f8969b5247cefda0d0829663c1355b4
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-04-11 19:08:04 +0200

    Request permissions on runtime

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/MainActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/MainActivity.java
@@ -5,10 +5,13 @@ import es.craftsmanship.toledo.katangapp.models.QueryResult;
 import es.craftsmanship.toledo.katangapp.utils.AndroidBus;
 import es.craftsmanship.toledo.katangapp.utils.KatangaFont;
 
+import android.Manifest;
+
 import android.app.Activity;
 import android.app.Dialog;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 
 import android.graphics.Typeface;
 
@@ -17,6 +20,8 @@ import android.location.Location;
 import android.os.Bundle;
 
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import android.util.Log;
 
@@ -120,6 +125,34 @@ public class MainActivity extends Activity
             googleApiClient, GPS_REQUEST, this);
     }
 
+    private void checkRuntimePermissions(String[] permissions, int requestCode) {
+        boolean explanation = false;
+
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                    // Show an explanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                    explanation = true;
+                }
+                else {
+                    // No explanation needed, we can request the permission.
+
+                    // requestCode is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }
+        }
+
+        if (!explanation) {
+            ActivityCompat.requestPermissions(this, permissions, requestCode);
+        }
+    }
+
     @Override
     public void onLocationChanged(Location location) {
         if (location != null) {
@@ -160,6 +193,8 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        initializeRuntimePermissions();
 
         initializeGooglePlayServices();
 
@@ -239,6 +274,17 @@ public class MainActivity extends Activity
             .addOnConnectionFailedListener(this)
             .addApi(LocationServices.API)
             .build();
+    }
+
+    private void initializeRuntimePermissions() {
+        int requestCode = PackageManager.PERMISSION_GRANTED;
+
+        String[] permissions = {
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.INTERNET
+        };
+
+        checkRuntimePermissions(permissions, requestCode);
     }
 
     /**
