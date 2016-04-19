5ea99be9190fab3bdcb77293b62a63e7fe37b53c
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-04-19 07:37:17 +0200

    [#17] Create a very basic info activity
    It will display info and help messages

---
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/InfoActivity.java
@@ -0,0 +1,18 @@
+package es.craftsmanship.toledo.katangapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_info);
+    }
+
+}
