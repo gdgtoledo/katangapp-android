eabca3a70ffd232010cf955dba61427585812696
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-04-20 09:57:05 +0200

    [#19] Minimally initialize the swipe refresh layout

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/ShowStopsActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/ShowStopsActivity.java
@@ -5,8 +5,11 @@ import es.craftsmanship.toledo.katangapp.models.QueryResult;
 
 import android.content.Intent;
 
+import android.graphics.Color;
+
 import android.os.Bundle;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,6 +24,7 @@ import java.util.List;
 public class ShowStopsActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +49,43 @@ public class ShowStopsActivity extends AppCompatActivity {
 
             recyclerView = (RecyclerView) findViewById(R.id.stops);
 
+            swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+
+            initializeSwipeRefreshLayout(busStopResults);
+
             processResults(busStopResults);
         }
         else {
             processEmptyResults();
         }
+    }
+
+    private void initializeSwipeRefreshLayout(final List<BusStopResult> busStopResults) {
+        swipeRefreshLayout.setColorSchemeColors(
+            Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW);
+
+        swipeRefreshLayout.setRefreshing(true);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            @Override
+            public void onRefresh() {
+                processResults(busStopResults);
+            }
+
+        });
+
+        final LinearLayoutManager layoutParams = new LinearLayoutManager(this);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                swipeRefreshLayout.setEnabled(
+                    layoutParams.findFirstCompletelyVisibleItemPosition() == 0);
+            }
+
+        });
     }
 
     private void processEmptyResults() {
