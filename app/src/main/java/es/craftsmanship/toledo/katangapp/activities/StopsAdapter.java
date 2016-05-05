c071b2795d474de186224c1694a249f58bbbdb29
Javier Gamarra <nhpatt@gmail.com>
2016-05-05 16:17:50 +0200

    good locale conversion :P

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/StopsAdapter.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/StopsAdapter.java
@@ -81,7 +81,7 @@ public class StopsAdapter extends RecyclerView.Adapter<StopsAdapter.StopsViewHol
 
             address.setText(busStop.getAddress());
 
-            String sb = "(" + String.format("%.2f", stop.getDistance(), Locale.US) + " " +
+            String sb = "(" + String.format( Locale.US, "%.2f", stop.getDistance()) + " " +
                 context.getString(R.string.meters) + ")";
 
             distance.setText(sb);
