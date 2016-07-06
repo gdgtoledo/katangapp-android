0fe07db93e8ac0d9b0006aa5d6ea0129d1767bb0
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-07-06 17:39:27 +0200

    [#42] Remove not needed comments

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/SplashActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/SplashActivity.java
@@ -14,7 +14,6 @@ import es.craftsmanship.toledo.katangapp.utils.KatangaFont;
  */
 public class SplashActivity extends AppCompatActivity {
 
-    // Splash screen timer
     private static int SPLASH_TIME_OUT = 2000;
 
     private TextView title;
@@ -33,20 +32,12 @@ public class SplashActivity extends AppCompatActivity {
 
         new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
-
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
                 Intent i = new Intent(SplashActivity.this, MainActivity.class);
 
                 startActivity(i);
 
-                // close this activity
                 finish();
             }
 
