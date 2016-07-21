f382d452b74c097b932a23ece3d2901bbe371748
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-07-21 18:15:58 +0200

    [#25] Add the contract that defines the favorites table

--- app/src/main/java/es/craftsmanship/toledo/katangapp/db/FavoritesContract.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/db/FavoritesContract.java
@@ -1,10 +1,20 @@
 package es.craftsmanship.toledo.katangapp.db;
 
+import android.provider.BaseColumns;
+
 /**
  * @author Manuel de la Peña
  */
 public final class FavoritesContract {
 
     public FavoritesContract() {}
+
+    public static abstract class Favorite implements BaseColumns {
+
+        public static final String TABLE_NAME = "favorite";
+        public static final String COLUMN_NAME_FAVORITE_ID = "favoriteId";
+        public static final String COLUMN_NAME_BUS_STOP_ID = "busStopId";
+
+    }
 
 }
