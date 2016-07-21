03c387418c1df10a1b1e163fe5dcc5566757d74c
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-07-21 18:26:37 +0200

    [#25] Define DDL sentences for the favorites

--- app/src/main/java/es/craftsmanship/toledo/katangapp/db/FavoritesContract.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/db/FavoritesContract.java
@@ -9,10 +9,20 @@ public final class FavoritesContract {
 
     public FavoritesContract() {}
 
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_FAVORITES =
+        "CREATE TABLE " + Favorite.TABLE_NAME + " (" +
+            Favorite._ID + " INTEGER PRIMARY KEY" + COMMA_SEP + " " +
+            Favorite.COLUMN_NAME_BUS_STOP_ID + " " + TEXT_TYPE +
+        " )";
+
+    private static final String SQL_DELETE_FAVORITES =
+        "DROP TABLE IF EXISTS " + Favorite.TABLE_NAME;
+
     public static abstract class Favorite implements BaseColumns {
 
         public static final String TABLE_NAME = "favorite";
-        public static final String COLUMN_NAME_FAVORITE_ID = "favoriteId";
         public static final String COLUMN_NAME_BUS_STOP_ID = "busStopId";
 
     }
