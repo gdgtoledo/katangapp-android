7d0640e7f4d8f9316c0aba71fb4020b2f1653da0
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-07-23 10:59:44 +0200

    [#25] Pass the favorite bus stop when subscribing to favorites

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/FavoritesActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/FavoritesActivity.java
@@ -3,6 +3,7 @@ package es.craftsmanship.toledo.katangapp.activities;
 import es.craftsmanship.toledo.katangapp.adapters.FavoritesAdapter;
 import es.craftsmanship.toledo.katangapp.db.FavoriteDAO;
 import es.craftsmanship.toledo.katangapp.db.model.Favorite;
+import es.craftsmanship.toledo.katangapp.models.BusStopResult;
 import es.craftsmanship.toledo.katangapp.models.QueryResult;
 
 import android.content.Intent;
@@ -29,6 +30,13 @@ public class FavoritesActivity extends BaseAndroidBusRegistrableActivity {
 
         intent.putExtra("queryResult", queryResult);
         intent.putExtra("favorites", true);
+
+        List<BusStopResult> results = queryResult.getResults();
+
+        // there should be only one
+        BusStopResult busStopResult = results.get(0);
+
+        intent.putExtra("busStop", busStopResult.getBusStop());
 
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
