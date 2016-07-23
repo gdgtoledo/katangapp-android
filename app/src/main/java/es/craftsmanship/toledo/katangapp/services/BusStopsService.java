dc1f58025c63992b93be97b77aac9cb760531925
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-07-23 09:30:14 +0200

    [#25] Add a new http service call to the favorites endpoint

--- app/src/main/java/es/craftsmanship/toledo/katangapp/services/BusStopsService.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/services/BusStopsService.java
@@ -4,6 +4,7 @@ import es.craftsmanship.toledo.katangapp.models.QueryResult;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -14,4 +15,8 @@ public interface BusStopsService {
     @GET("main")
     Call<QueryResult> listBusStops(
         @Query("lt") Double latitude, @Query("ln") Double longitude, @Query("r") String radius);
+
+    @GET("favorite/{busStopId}")
+    Call<QueryResult> favorites(@Path("busStopId") String busStopId);
+
 }
