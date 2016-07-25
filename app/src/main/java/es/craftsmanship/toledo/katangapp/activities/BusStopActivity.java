0fdeb1e0773f19aca59160ed20b77f28958ee7ad
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-07-25 09:42:22 +0200

    [#25] Support returning back to parent from bus stop activity

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/BusStopActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/BusStopActivity.java
@@ -12,10 +12,12 @@ import android.os.Bundle;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
 /**
@@ -90,6 +92,18 @@ public class BusStopActivity extends AppCompatActivity {
 
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private Drawable getFavoritedDrawable(boolean favorited) {
