dda6f050ab54b3a731526e47e2584894a39622f4
Cristobal Hermida <chermidap@gmail.com>
2016-08-07 13:01:22 +0200

    [#14] new Activity for showing maps and  stop list

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/RouteMapActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/RouteMapActivity.java
@@ -1,13 +1,16 @@
 package es.craftsmanship.toledo.katangapp.activities;
 
-import android.*;
-import android.app.Dialog;
+import es.craftsmanship.toledo.katangapp.fragments.MapaFragment;
+import es.craftsmanship.toledo.katangapp.fragments.RouteFragment;
+import es.craftsmanship.toledo.katangapp.models.BusStop;
+import es.craftsmanship.toledo.katangapp.models.QueryResult;
+import es.craftsmanship.toledo.katangapp.models.Route;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
+
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -15,18 +18,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -35,52 +31,45 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 import java.util.List;
 
-import es.craftsmanship.toledo.katangapp.fragments.MapaFragment;
-import es.craftsmanship.toledo.katangapp.fragments.RouteFragment;
-import es.craftsmanship.toledo.katangapp.models.BusStop;
-import es.craftsmanship.toledo.katangapp.models.QueryResult;
-import es.craftsmanship.toledo.katangapp.models.Route;
+
 
 
 public class RouteMapActivity extends BaseGeoLocatedActivity implements OnMapReadyCallback {
 
-    private static final String TAG = "KATANGAPP";
+    private static final String CTE_MAPA = "MAPA";
+    private static final String CTE_RUTA = "RUTA";
 
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-    private static final int LOCATION_REQUEST_CODE = 1;
-
-    private ViewPager mViewPager;
-    private TabLayout tabLayout;
-    private String idRuta;
-    private List<BusStop> listStops;
-    private MapaFragment mFirstMapFragment;
-    List<BusStop> busStopResults;
     private GoogleMap mMap;
+    private MapaFragment mFirstMapFragment;
+    private List<BusStop> busStopResults;
+    private TabLayout tabLayout;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_map);
 
-      //  initializeRuntimePermissions();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         Intent intent = getIntent();
         if (intent.hasExtra("ruta") &&
                 (intent.getSerializableExtra("ruta") != null)) {
             Route route = (Route) intent.getSerializableExtra("ruta");
             setTitle(route.getName());
             busStopResults = route.getBusStops();
-
+            mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(),route);
+        }else {
+            mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(),null);
         }
-        mSectionsPagerAdapter.setRuta(busStopResults);
-        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
@@ -91,33 +80,14 @@ public class RouteMapActivity extends BaseGeoLocatedActivity implements OnMapRea
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_route_map, menu);
         return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == android.R.id.home) {
-            Intent intent = new Intent(getApplicationContext(), ShowStopsActivity.class);
-            startActivity(intent);
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
-
-       // mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
         mMap.getUiSettings().setMapToolbarEnabled(true);
@@ -132,8 +102,13 @@ public class RouteMapActivity extends BaseGeoLocatedActivity implements OnMapRea
                         .title(stop.getAddress()));
             }
         }
-
-     /*   LatLng locStopIni = new LatLng( getLatitude(), getLongitude());
+        LatLng locStopIni=null;
+        if(getLatitude()!=null && getLongitude()!=null){
+            locStopIni  = new LatLng( getLatitude(), getLongitude());
+        }else{
+            BusStop busStop=   busStopResults.get(0);
+            locStopIni  = new LatLng(  busStop.getLatitude(), busStop.getLongitude());
+        }
 
         mMap.addMarker(new MarkerOptions()
                 .position(locStopIni)
@@ -144,49 +119,19 @@ public class RouteMapActivity extends BaseGeoLocatedActivity implements OnMapRea
                 .zoom(12)
                 .build();
 
-        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));*/
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-
-    private void initializeRuntimePermissions() {
-        int requestCode = PackageManager.PERMISSION_GRANTED;
-
-        String[] permissions = {
-                android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.INTERNET
-        };
-
-        checkRuntimePermissions(permissions, requestCode);
-    }
-
-    private void checkRuntimePermissions(String[] permissions, int requestCode) {
-        boolean explanation = false;
-
-        for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) !=
-                    PackageManager.PERMISSION_GRANTED) {
-
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
-                    // Show an explanation to the user *asynchronously* -- don't block
-                    // this thread waiting for the user's response! After the user
-                    // sees the explanation, try again to request the permission.
-                    explanation = true;
-                }
-                else {
-                    // No explanation needed, we can request the permission.
-
-                    // requestCode is an
-                    // app-defined int constant. The callback method gets the
-                    // result of the request.
-                }
-            }
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            Intent intent = new Intent(getApplicationContext(), ShowBusStopsActivity.class);
+            startActivity(intent);
         }
-
-        if (!explanation) {
-            ActivityCompat.requestPermissions(this, permissions, requestCode);
-        }
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -201,48 +146,41 @@ public class RouteMapActivity extends BaseGeoLocatedActivity implements OnMapRea
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public List<BusStop> ruta;
-        public SectionsPagerAdapter(FragmentManager fm) {
+        public Route route;
+
+        public SectionsPagerAdapter(FragmentManager fm, Route route) {
             super(fm);
+            this.route=route;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
         }
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a RouteFragment (defined as a static inner class below).
 
             switch (position) {
                 case 0:
-                    mFirstMapFragment = mFirstMapFragment.newInstance();
-                    mFirstMapFragment.getMapAsync(RouteMapActivity.this);
+                    mFirstMapFragment.newInstance().getMapAsync(RouteMapActivity.this);
                     return mFirstMapFragment;
 
                 case 1:
-                    return RouteFragment.newInstance(ruta);
+                    return RouteFragment.newInstance(route);
 
             }
             return null;
 
-        }
-        public void setRuta(List<BusStop>listadoParadas){
-
-            ruta=listadoParadas;
-        }
-
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 2;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "MAPA";
+                    return CTE_MAPA;
                 case 1:
-                     return "RUTA";
+                    return CTE_RUTA;
             }
             return null;
         }
