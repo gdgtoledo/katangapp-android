3d73cfc9afe6be7c36fb60eb167df25000172c2b
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-08-08 10:37:29 +0200

    [#14] Apply interactor pattern to RoutesInteractor

--- app/src/main/java/es/craftsmanship/toledo/katangapp/interactors/RoutesInteractor.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/interactors/RoutesInteractor.java
@@ -1,19 +1,19 @@
 package es.craftsmanship.toledo.katangapp.interactors;
 
-import java.io.IOException;
-
 import es.craftsmanship.toledo.katangapp.models.Route;
 import es.craftsmanship.toledo.katangapp.services.BusStopsService;
-import es.craftsmanship.toledo.katangapp.utils.AndroidBus;
+import es.craftsmanship.toledo.katangapp.services.KatangaResponseWrapper;
+
+import java.io.IOException;
+
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 /**
  * @author Cristóbal Hermida
+ * @author Manuel de la Peña
  */
-public class RoutesInteractor implements Runnable{
-    private static final String BACKEND_ENDPOINT = "https://secret-depths-4660.herokuapp.com";
+public class RoutesInteractor extends BaseKatangaInteractor {
+
     private final String route;
 
     public RoutesInteractor(String route) {
@@ -21,30 +21,12 @@ public class RoutesInteractor implements Runnable{
     }
 
     @Override
-    public void run() {
-        try {
-            Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BACKEND_ENDPOINT)
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
+    protected KatangaResponseWrapper<Response<Route>> getResponse(BusStopsService busStopsService)
+        throws IOException {
 
-            BusStopsService service = retrofit.create(BusStopsService.class);
+        Response<Route> response = busStopsService.getRouteId(route).execute();
 
-            Response<Route> response = null;
-
-            response = service.getRouteId(route).execute();
-
-            Object event = new Error(response.message());
-
-            if (response.isSuccessful()) {
-                event = response.body();
-            }
-
-            AndroidBus.getInstance().post(event);
-        }
-        catch (IOException e) {
-            AndroidBus.getInstance().post(e);
-        }
+        return new KatangaResponseWrapper<>(response);
     }
 
 }
