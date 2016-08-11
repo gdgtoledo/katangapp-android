9b79b99ddc8832323ffc26fcf45e53318e30467d
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-08-11 14:03:51 +0200

    [#66] Go to bus stop screen when clicking on the label or the icon

--- app/src/main/java/es/craftsmanship/toledo/katangapp/adapters/RouteBusStopsAdapter.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/adapters/RouteBusStopsAdapter.java
@@ -1,18 +1,22 @@
 package es.craftsmanship.toledo.katangapp.adapters;
 
 import es.craftsmanship.toledo.katangapp.R;
+import es.craftsmanship.toledo.katangapp.activities.BusStopActivity;
 import es.craftsmanship.toledo.katangapp.models.BusStop;
 import es.craftsmanship.toledo.katangapp.utils.KatangaFont;
 
 import android.content.Context;
+import android.content.Intent;
 
 import android.graphics.Typeface;
 
 import android.support.v7.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -24,6 +28,7 @@ public class RouteBusStopsAdapter
     extends RecyclerView.Adapter<RouteBusStopsAdapter.RouteBusStopsViewHolder>  {
 
     private final List<BusStop> busStops;
+    private Context context;
 
     public RouteBusStopsAdapter(List<BusStop> busStops) {
         this.busStops = busStops;
@@ -38,6 +43,8 @@ public class RouteBusStopsAdapter
     public RouteBusStopsViewHolder onCreateViewHolder(
         ViewGroup parent, int viewType) {
 
+        context = parent.getContext();
+
         return new RouteBusStopsViewHolder(parent);
     }
 
@@ -48,27 +55,45 @@ public class RouteBusStopsAdapter
 
     public class RouteBusStopsViewHolder extends RecyclerView.ViewHolder {
 
+        private BusStop currentBusStop;
         private TextView address;
-        private final ViewGroup parent;
+        private ImageView busStopIcon;
 
         public RouteBusStopsViewHolder(ViewGroup parent) {
             super(
                 LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.bus_line_row, parent, false));
 
-            this.parent = parent;
-
-            Context context = this.parent.getContext();
-
             address = (TextView) itemView.findViewById(R.id.bus_stop_address);
+            busStopIcon = (ImageView) itemView.findViewById(R.id.bus_stop_icon);
 
             Typeface tf = KatangaFont.getFont(context.getAssets(), KatangaFont.QUICKSAND_REGULAR);
 
             address.setTypeface(tf);
+
+            View.OnClickListener busStopClickListener = new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, BusStopActivity.class);
+
+                    intent.putExtra("busStop", currentBusStop);
+
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                    context.startActivity(intent);
+                }
+
+            };
+
+            address.setOnClickListener(busStopClickListener);
+            busStopIcon.setOnClickListener(busStopClickListener);
         }
 
         public void bind(BusStop busStop) {
-            address.setText(busStop.getAddress());
+            currentBusStop = busStop;
+
+            address.setText(currentBusStop.getAddress());
         }
 
     }
