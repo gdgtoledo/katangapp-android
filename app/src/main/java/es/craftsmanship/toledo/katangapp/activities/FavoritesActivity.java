gdgtoledo/katangapp-android

commit ace61e33cebe74273375a56b68ac2243419f4032

[#69] rename favourites to use same convention

app/src/main/java/es/craftsmanship/toledo/katangapp/activities/FavoritesActivity.java
@@ -32,7 +32,7 @@ public class FavoritesActivity extends BaseAndroidBusRegistrableActivity {
         Intent intent = new Intent(FavoritesActivity.this, ShowBusStopsActivity.class);
 
         intent.putExtra("queryResult", queryResult);
-        intent.putExtra("favorites", true);
+        intent.putExtra("activity_favorites", true);
 
         List<BusStopResult> results = queryResult.getResults();
 
@@ -50,7 +50,7 @@ public class FavoritesActivity extends BaseAndroidBusRegistrableActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.favorites);
+        setContentView(R.layout.activity_favorites);
 
         try(FavoriteDAO favoriteDAO = new FavoriteDAO(this)) {
             favoriteDAO.open();

app/src/main/res/layout/activity_favorites.xml
@@ -2,8 +2,7 @@
 <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
     android:orientation="vertical"
     android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    android:background="@color/katanga_grey_dark">
+    android:layout_height="match_parent">
 
     <android.support.v7.widget.RecyclerView
         android:id="@+id/favoritesList"

commit d7eb4b8c71b1c8c3d964f4220a380ac7ca6e65f5

[#70] Fix regression on swipe
Caused by ace61e33cebe74273375a56b68ac2243419f4032

app/src/main/java/es/craftsmanship/toledo/katangapp/activities/FavoritesActivity.java
@@ -7,6 +7,7 @@ import es.craftsmanship.toledo.katangapp.db.FavoriteDAO;
 import es.craftsmanship.toledo.katangapp.db.model.Favorite;
 import es.craftsmanship.toledo.katangapp.models.BusStopResult;
 import es.craftsmanship.toledo.katangapp.models.QueryResult;
+import es.craftsmanship.toledo.katangapp.utils.ExtrasConstants;
 
 import android.content.Intent;
 
@@ -32,7 +33,7 @@ public class FavoritesActivity extends BaseAndroidBusRegistrableActivity {
         Intent intent = new Intent(FavoritesActivity.this, ShowBusStopsActivity.class);
 
         intent.putExtra("queryResult", queryResult);
-        intent.putExtra("activity_favorites", true);
+        intent.putExtra(ExtrasConstants.ACTIVITY_FAVORITES, true);
 
         List<BusStopResult> results = queryResult.getResults();
 

app/src/main/java/es/craftsmanship/toledo/katangapp/interactors/KatangaInteractorFactoryUtil.java
@@ -3,6 +3,7 @@ package es.craftsmanship.toledo.katangapp.interactors;
 import android.os.Bundle;
 
 import es.craftsmanship.toledo.katangapp.models.BusStop;
+import es.craftsmanship.toledo.katangapp.utils.ExtrasConstants;
 
 /**
  * @author Manuel de la Peña
@@ -17,7 +18,7 @@ public class KatangaInteractorFactoryUtil implements KatangaInteractorFactory {
     public KatangaInteractor getInteractor(Bundle extras) throws InvalidInteractorException {
         validate(extras);
 
-        boolean favorites = extras.getBoolean("favorites");
+        boolean favorites = extras.getBoolean(ExtrasConstants.ACTIVITY_FAVORITES);
 
         if (favorites) {
             BusStop busStop = (BusStop) extras.getSerializable("busStop");
@@ -53,7 +54,9 @@ public class KatangaInteractorFactoryUtil implements KatangaInteractorFactory {
      * @throws InvalidInteractorException if any validation condition is not satisfied
      */
     private void validate(Bundle extras) throws InvalidInteractorException {
-        if (extras.containsKey("favorites") && extras.getBoolean("favorites")) {
+        if (extras.containsKey(ExtrasConstants.ACTIVITY_FAVORITES) &&
+            extras.getBoolean(ExtrasConstants.ACTIVITY_FAVORITES)) {
+
             if (!extras.containsKey("busStop") || (extras.getSerializable("busStop") == null)) {
                 throw new InvalidInteractorException("Favorite Bus Stop is not present.");
             }

app/src/main/java/es/craftsmanship/toledo/katangapp/utils/ExtrasConstants.java
@@ -1,0 +1,10 @@
+package es.craftsmanship.toledo.katangapp.utils;
+
+/**
+ * @author Manuel de la Peña
+ */
+public class ExtrasConstants {
+
+    public static final String ACTIVITY_FAVORITES = "activity_favorites";
+
+}

app/src/test/java/es/craftsmanship/toledo/katangapp/interactors/KatangaInteractorFactoryUtilTest.java
@@ -1,6 +1,7 @@
 package es.craftsmanship.toledo.katangapp.interactors;
 
 import es.craftsmanship.toledo.katangapp.models.BusStop;
+import es.craftsmanship.toledo.katangapp.utils.ExtrasConstants;
 
 import android.os.Bundle;
 
@@ -85,8 +86,8 @@ public class KatangaInteractorFactoryUtilTest {
     public void testGetFavoritesInteractorWithFalseFavoritesFlag() throws Exception {
         KatangaInteractorFactoryUtil instance = KatangaInteractorFactoryUtil.getInstance();
 
-        Mockito.when(extras.containsKey("favorites")).thenReturn(true);
-        Mockito.when(extras.getBoolean("favorites")).thenReturn(false);
+        Mockito.when(extras.containsKey(ExtrasConstants.ACTIVITY_FAVORITES)).thenReturn(true);
+        Mockito.when(extras.getBoolean(ExtrasConstants.ACTIVITY_FAVORITES)).thenReturn(false);
 
         exception.expect(InvalidInteractorException.class);
         exception.expectMessage("Main search parameters (lat, long, radio) are not present.");
@@ -132,7 +133,7 @@ public class KatangaInteractorFactoryUtilTest {
     }
 
     private void mockGetBusStopsInteractor(boolean radio, boolean latitude, boolean longitude) {
-        Mockito.when(extras.containsKey("favorites")).thenReturn(false);
+        Mockito.when(extras.containsKey(ExtrasConstants.ACTIVITY_FAVORITES)).thenReturn(false);
 
         Mockito.when(extras.containsKey("radio")).thenReturn(radio);
         Mockito.when(extras.containsKey("latitude")).thenReturn(latitude);
@@ -140,8 +141,8 @@ public class KatangaInteractorFactoryUtilTest {
     }
 
     private void mockGetFavoritesInteractor(boolean favorites, boolean busStop) {
-        Mockito.when(extras.containsKey("favorites")).thenReturn(favorites);
-        Mockito.when(extras.getBoolean("favorites")).thenReturn(favorites);
+        Mockito.when(extras.containsKey(ExtrasConstants.ACTIVITY_FAVORITES)).thenReturn(favorites);
+        Mockito.when(extras.getBoolean(ExtrasConstants.ACTIVITY_FAVORITES)).thenReturn(favorites);
 
         Mockito.when(extras.containsKey("busStop")).thenReturn(busStop);
     }