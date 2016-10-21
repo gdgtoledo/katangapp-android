e3b86f89d1d0329143a6c18ba0d4f2238093a3ec
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-10-21 13:50:43 +0200

    [#64] Populate activity layout with data passed from the intent, using the already created adapter

--- app/src/main/java/es/craftsmanship/toledo/katangapp/activities/RoutesActivity.java
+++ app/src/main/java/es/craftsmanship/toledo/katangapp/activities/RoutesActivity.java
@@ -1,15 +1,80 @@
 package es.craftsmanship.toledo.katangapp.activities;
 
+import es.craftsmanship.toledo.katangapp.R;
+import es.craftsmanship.toledo.katangapp.adapters.RoutesAdapter;
+import es.craftsmanship.toledo.katangapp.models.Route;
+import es.craftsmanship.toledo.katangapp.subscribers.RouteSubscriber;
+import es.craftsmanship.toledo.katangapp.subscribers.RoutesSubscriber;
+import es.craftsmanship.toledo.katangapp.utils.ExtrasConstants;
+
+import android.content.Intent;
+
 import android.os.Bundle;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.squareup.otto.Subscribe;
 
 /**
  * @author Manuel de la Peña
  */
-public class RoutesActivity extends BaseAndroidBusRegistrableActivity {
+public class RoutesActivity
+    extends BaseAndroidBusRegistrableActivity
+    implements RouteSubscriber, RoutesSubscriber {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    @Subscribe
+    public void routeReceived(Error error) {
+    }
+
+    @Override
+    @Subscribe
+    public void routeReceived(Route route) {
+        if (route != null) {
+            Intent intent = new Intent(this, RouteMapActivity.class);
+
+            intent.putExtra(ExtrasConstants.ROUTE, route);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    @Subscribe
+    public void routesReceived(Error error) {
+    }
+
+    @Override
+    @Subscribe
+    public void routesReceived(Route[] routes) {
+        recyclerView.setAdapter(new RoutesAdapter(routes));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_routes);
+
+        Intent intent = getIntent();
+
+        if (intent.hasExtra(ExtrasConstants.ROUTES) &&
+            (intent.getSerializableExtra(ExtrasConstants.ROUTES) != null)) {
+
+            Route[] routes = (Route[]) intent.getSerializableExtra(
+                ExtrasConstants.ROUTES);
+
+            recyclerView = (RecyclerView) findViewById(R.id.routesList);
+
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new RoutesAdapter(routes));
+        }
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
